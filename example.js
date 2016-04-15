b12dc85563866332113a112f72f5c8889696b7e8
Byrdann Fox <byrdannfox@gmail.com>
2016-04-15 12:53:27 -0400

    added babel utils, eslint setup, es6 syntax and getting ready to finish off with some unit tests with mocha at the very least...

--- example.js
+++ example.js
@@ -1,8 +1,9 @@
 'use strict';
 
-// run 'nodemon example.js' in your terminal to play around.
+// run 'npm start' in your terminal to play around.
+// run 'npm test' in your terminal to test the module.
 
-var regexmatcher = require('./regexmatcher');
-
-var matches = regexmatcher.getMatches(/https?/gi, 'http HTTPS httpS SPTTH https');
-console.log(matches); // { matches: [ [ 'http', index: 0, input: 'http HTTPS httpS SPTTH https' ], [ 'HTTPS', index: 5, input: 'http HTTPS httpS SPTTH https' ], [ 'httpS', index: 11, input: 'http HTTPS httpS SPTTH https' ], [ 'https', index: 23, input: 'http HTTPS httpS SPTTH https' ] ], summary: 'There are 4 matches' }
+const regexmatcher = require('./regexmatcher'),
+    matches = regexmatcher.getMatches(/https?/gi, 'http HTTPS httpS SPTTH https');
+console.log(matches);
+// { matches: [ [ 'http', index: 0, input: 'http HTTPS httpS SPTTH https' ], [ 'HTTPS', index: 5, input: 'http HTTPS httpS SPTTH https' ], [ 'httpS', index: 11, input: 'http HTTPS httpS SPTTH https' ], [ 'https', index: 23, input: 'http HTTPS httpS SPTTH https' ] ], summary: 'There are 4 matches' }
--- regexmatcher.js
+++ regexmatcher.js
@@ -1,31 +1,32 @@
 'use strict';
 
-var _ = require('lodash');
+import _ from 'lodash';
 
-module.exports.getMatches = function (regex, string) {
-		var deep = _.cloneDeep([regex]),
-		regexClone = deep[0],
-		results = [],
-		resultsContainer = {},
-		match, 
-		currentMatchIndex;
+module.exports.getMatches = (regex, string) => {
+    const deep = _.cloneDeep([regex]),
+        regexClone = deep[0],
+        results = [],
+        resultsContainer = {};
 
-	while (regex.exec(string) !== null) {
-		match = regexClone.exec(string);
-		if (currentMatchIndex === undefined) {
-			currentMatchIndex = match.index;
-			results.push(match);
-		} else if (match.index > currentMatchIndex) {
-			currentMatchIndex = match.index;
-			results.push(match);	
-		} else {
-			break;
-		}
-	}
+    var match,
+        currentMatchIndex;
 
-	resultsContainer.matches = results;
+    while (regex.exec(string) !== null) {
+        match = regexClone.exec(string);
+        if (currentMatchIndex === undefined) {
+            currentMatchIndex = match.index;
+            results.push(match);
+        } else if (match.index > currentMatchIndex) {
+            currentMatchIndex = match.index;
+            results.push(match);
+        } else {
+            break;
+        }
+    }
 
-	(results.length === 1) ? resultsContainer.summary = 'There is 1 match.' : resultsContainer.summary = 'There are ' + results.length + ' matches';
+    resultsContainer.matches = results;
 
-	return resultsContainer;
+    (results.length === 1) ? resultsContainer.summary = 'There is 1 match.' : resultsContainer.summary = `There are ${results.length} matches`;
+
+    return resultsContainer;
 };
